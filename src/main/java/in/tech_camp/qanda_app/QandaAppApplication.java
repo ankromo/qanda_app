d25e126b25ad88028b07367e10403e9ffe027c2f
ankromo <ankro.mocchic5@gmail.com>
2026-06-17 10:24:17 +0900

    Initial commit

---
+++ src/main/java/in/tech_camp/qanda_app/QandaAppApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.qanda_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QandaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QandaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/tech_camp/qanda_app/QandaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.qanda_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QandaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
